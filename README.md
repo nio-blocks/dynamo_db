209a330c6257df5668df43fcb56ca438b669322f
Chris Brackert <cbrackert@gmail.com>
2015-03-16 10:45:56 -0700

    Init DynamoDBQuery block

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-DynamoDB
-===========
+DynamoDBInsert
+==============
 
-Save signals to an AWS DynamoDB
+Save signals to an AWS DynamoDB.
 
 Properties
---------------
+----------
 
  - **Table**: (expression) The name of the table to save to
  - **AWS Region**: The AWS region to save to
@@ -15,16 +15,16 @@ Properties
 
 
 Dependencies
-----------------
+------------
 
  * boto - https://github.com/boto/boto
 
 Commands
-----------------
+--------
 None
 
 Input
--------
+-----
 Any list of signals.
 
 Signals must have the attribute of the **hash_key** defined. If a range key is also specified, then the signals must also contain that as an attribute.
@@ -32,5 +32,45 @@ Signals must have the attribute of the **hash_key** defined. If a range key is a
 Note that errors may occur if the type of the hash key attribute is different than the type of the hash key on the DynamoDB table.
 
 Output
----------
+------
 None
+
+-------------------------------------------------------------------------------
+
+DynamoDBQuery
+=============
+
+Read signals from AWS DynamoDB.
+
+**Filter Keys** must be of the format `<fieldname>__<filter_operation>`. Options for `filter_operations` are eq, lt, lte, gt, gte, between and (for strings only) beginswith.
+
+A query for the hash key equal to a value must be present in the query. For example, if `name` is the hash key, then one **Filter Key** must be `name__eq`.
+
+Note that errors may occur if the type of the table attribute is different than the type of the **Filter Value**.
+
+Properties
+----------
+
+ - **Table**: (expression) The name of the table to save to
+ - **AWS Region**: The AWS region to save to
+ - **Access Key**: An AWS Access Token
+ - **Access Secret**: The AWS Access Token's secret
+ - **Query Filters**: Key/value pairs for query (Default key='id__eq' value='{{ $id }}')
+
+
+Dependencies
+------------
+
+ * boto - https://github.com/boto/boto
+
+Commands
+--------
+None
+
+Input
+-----
+Any list of signals.
+
+Output
+------
+One signal for each result in the ResultSet from the query.
---
+++ tests/test_dynamo_db_query_block.py
@@ -0,0 +1,86 @@
+from unittest.mock import MagicMock, patch
+from nio.common.signal.base import Signal
+from nio.util.support.block_test_case import NIOBlockTestCase
+from ..dynamo_db_query_block import DynamoDBQuery
+from ..dynamo_db_base_block import DynamoDBBase
+
+
+class SaveCounterDynamoDB(DynamoDBQuery):
+
+    def __init__(self):
+        super().__init__()
+        self._count = 0
+
+    def execute_signals_query(self, table, signals):
+        self._count += 1
+        super().execute_signals_query(table, signals)
+
+
+@patch(DynamoDBBase.__module__ + '.connect_to_region')
+@patch(DynamoDBBase.__module__ + '.Table.count')
+@patch(DynamoDBBase.__module__ + '.Table.query_2')
+class TestDynamoDBQuery(NIOBlockTestCase):
+
+    def test_build_query_dict_default(self, q_func, count_func, connect_func):
+        blk = DynamoDBQuery()
+        self.configure_block(blk, {})
+        query_dict = blk._build_query_dict(Signal({'id': 1}))
+        self.assertDictEqual(query_dict, {
+            'id__eq': 1
+        })
+
+    def test_build_query_dict_multiple(self, q_func, count_func, connect_func):
+        blk = DynamoDBQuery()
+        self.configure_block(blk, {
+            'query_filters': [
+                {'key': 'str__eq', 'value': '1'},
+                {'key': 'dup__eq', 'value': 'first'},
+                {'key': 'dup__eq', 'value': 'duplicate'},
+                {'key': 'int__eq', 'value': '{{ 1 }}' },
+                {'key': 'exp__eq', 'value': "{{ $id }}"}
+            ]
+        })
+        query_dict = blk._build_query_dict(Signal({'id': 'signal'}))
+        self.assertDictEqual(query_dict, {
+            'str__eq': '1',
+            'dup__eq': 'duplicate',
+            'int__eq': 1,
+            'exp__eq': 'signal'
+        })
+
+    def test_build_query_dict_fail(self, q_func, count_func, connect_func):
+        blk = DynamoDBQuery()
+        self.configure_block(blk, {
+            'query_filters': [
+                {'key': 'good__eq', 'value': 'good'},
+                {'key': 'bad__eq', 'value': "{{ $id }}"},
+                {'key': 'moregood__eq', 'value': 'moregood'}
+            ]
+        })
+        self.assertRaises(Exception, blk._build_query_dict, Signal())
+
+    def test_query_table(self, q_func, count_func, connect_func):
+        blk = DynamoDBQuery()
+        self.configure_block(blk, {
+            'query_filters': [
+                {'key': 'id__eq', 'value': '{{ $id }}'},
+                {'key': 'str__beginswith', 'value': 'begin'}
+            ]
+        })
+        # have the query return two results
+        q_func.return_value = [{}, {}]
+        blk.process_signals([Signal({'id': 1})])
+        q_func.assert_called_once_with(id__eq=1,
+                                      str__beginswith='begin')
+        self.assert_num_signals_notified(2)
+
+    def test_query_multiple(self, q_func, count_func, connect_func):
+        blk = DynamoDBQuery()
+        self.configure_block(blk, {})
+        # have each query return one result
+        q_func.return_value = [{}]
+        blk.process_signals([Signal({'id': 1}),
+                             Signal(),
+                             Signal({'id': 1})])
+        # one of the three queries falied so two signals are notified
+        self.assert_num_signals_notified(2)
